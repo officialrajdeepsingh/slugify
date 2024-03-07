ecc92054d466a0ebb589979acfa6f2e33dcba607
Rajdeep singh <radewala.2@gmail.com>
2024-03-07 09:17:42 +0000

    move the src folder

--- main_test.ts
+++
@@ -1,6 +0,0 @@
-import { assertEquals } from "https://deno.land/std@0.207.0/assert/mod.ts";
-import { Slugify } from "./main.ts";
-
-Deno.test(function addTest() {
-  assertEquals(Slugify("Hello World"), "hello-world");
-});
